71fd7310140735319c2aa15802f1158d7367ae05
Travis <travis.borer@outlook.com>
2025-04-03 15:13:35 -0500

    Update next.config.mjs
    fixed missing closing bracket on next config mjs
    
    Signed-off-by: Travis <travis.borer@outlook.com>

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,11 @@ const nextConfig = {
   webpack: (config, context) => {
     config.optimization.minimize = process.env.NEXT_PUBLIC_CO_DEV_ENV !== "preview";
     return config;
+  },
   env: {
     EMAIL_USERNAME: process.env.EMAIL_USERNAME,
-    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,  
-  }
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+  },
 };
 
 export default nextConfig;
