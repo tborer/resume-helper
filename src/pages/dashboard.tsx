a48fff65449741f1a2e508b00424246e38f89c29
travis.borer <travis.borer@gmail.com>
2025-04-08 18:14:08 +0000

    count logic implementation

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -363,6 +363,31 @@ export default function Dashboard() {
       return;
     }
 
+    //code to check key and count
+    const { key: apiKey, isMasterKey } = getApiKeyToUse();
+    if (isMasterKey && userData.dailyAnalysisCount >= 10) {
+      alert("You have reached your daily limit of resume analyses. Add your own API key in the Account tab to remove this limit.");
+      return;
+    } else if (isMasterKey && userData.dailyAnalysisCount < 10) {
+      try {
+        const incrementResponse = await incrementDailyAnalysisCount(userData.email);
+        if (incrementResponse.ok) {
+          const incrementedData = await incrementResponse.json();
+          console.log('User can run analysis and incrementing count.');
+          setUserData({ ...userData, dailyAnalysisCount: incrementedData.dailyAnalysisCount });
+        } else {
+          const errorData = await incrementResponse.json();
+          console.error('Error incrementing dailyAnalysisCount:', errorData);
+          alert("There was an error with your daily analysis.");
+          return;
+        }
+      } catch (error) {
+        console.error("Error incrementing dailyAnalysisCount:", error);
+        alert("There was an error, please refresh and try again.");
+        return;
+      }
+    }
+
     /*
     //code to check key and count
     if (userData.geminiApiKey === null && userData.dailyAnalysisCount >= 10) {
