db465017755948614b50fe7b7d17b190598be7df
Travis <travis.borer@outlook.com>
2025-04-09 10:50:43 -0500

    Update _document.tsx
    GA tag
    
    Signed-off-by: Travis <travis.borer@outlook.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,14 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TXWZK2XVHY"></script>
+        <script dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());gtag('config', 'G-TXWZK2XVHY');`}} />
+      </Head>
       <body>
         <Main />
         <Script src="https://assets.co.dev/files/codevscript.js" strategy="afterInteractive" />
