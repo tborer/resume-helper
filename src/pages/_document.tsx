8e724d961351afa1b1158f1f67572e5f3aeaea79
Travis <travis.borer@outlook.com>
2025-04-09 10:43:10 -0500

    Update _document.tsx
    removing GA update
    
    Signed-off-by: Travis <travis.borer@outlook.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,17 +4,7 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-TXWZK2XVHY"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-TXWZK2XVHY');
-      </script>
-      </Head>
+      <Head/>
       <body>
         <Main />
         <Script src="https://assets.co.dev/files/codevscript.js" strategy="afterInteractive" />
